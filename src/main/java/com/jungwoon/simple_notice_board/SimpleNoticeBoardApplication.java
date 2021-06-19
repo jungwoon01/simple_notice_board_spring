5fccd04ce23092172ae0368901f839560d3f08fe
jungwoon <jungwoon018@gmail.com>
2021-06-19 16:51:45 +0900

    Make new project

---
+++ src/main/java/com/jungwoon/simple_notice_board/SimpleNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.jungwoon.simple_notice_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleNoticeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleNoticeBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jungwoon/simple_notice_board/SimpleNoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungwoon.simple_notice_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNoticeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

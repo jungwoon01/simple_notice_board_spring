adab3f752ce3c1ba5a89431977421ae10f315a7f
jungwoon <jungwoon018@gmail.com>
2021-06-20 20:11:10 +0900

    Make application.properties for test

--- src/test/java/com/jungwoon/simple_notice_board/domain/LikesRepositoryTests.java
+++ src/test/java/com/jungwoon/simple_notice_board/domain/LikesRepositoryTests.java
@@ -1,4 +1,19 @@
 package com.jungwoon.simple_notice_board.domain;
 
+import com.jungwoon.simple_notice_board.domain.posts.PostsRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class LikesRepositoryTests {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @Test
+    public void saveLikeTest() {
+        // given
+
+    }
 }
